bbb0e757bdf6bfae9b2739be540be419875f6ec0
Alex Orga <alexorga.dev@gmail.com>
2018-10-01 20:54:48 -0500

    added properties to clinker class

--- ClinkedIn/Models/Clinker.cs
+++ ClinkedIn/Models/Clinker.cs
@@ -7,5 +7,10 @@ namespace ClinkedIn.Models
 {
     public class Clinker
     {
+        public string Name { get; set; }
+        public string Nickname { get; set; }
+        public string Charge { get; set; }
+        public int Wallet { get; set; }
+        public int Id { get; set; }
     }
 }
