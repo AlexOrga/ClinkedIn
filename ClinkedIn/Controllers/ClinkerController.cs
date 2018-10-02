44592273fccf67e0fd3b3e5b37ff98165f1b7e7c
Nathan Pabst <nathan.pabst@gmail.com>
2018-10-02 18:34:40 -0500

    using diff branch

---
+++ ClinkedIn/Controllers/ClinkerController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ClinkedIn.DataAccess;
+using ClinkedIn.Models;
+
+
+namespace ClinkedIn.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClinkerController : ControllerBase
+    {
+
+        static List<Clinker> Clinkers;
+        static ClinkerController()
+        {
+
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Clinker>> GetAll()
+        {
+            return Clinkers;
+        }
+        
+
+    }
+}
