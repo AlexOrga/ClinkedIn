59200d84d9cb23c7fcac64d139711c2cbb7af01d
Alex Orga <alexorga.dev@gmail.com>
2018-10-02 18:29:39 -0500

    returning all clinkers from get request

---
+++ ClinkedIn/Controllers/ClinkerController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ClinkedIn.DataAccess;
+using ClinkedIn.Models;
+
+namespace ClinkedIn.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClinkerController : ControllerBase
+    {
+        public readonly ClinkerStorage _storage;
+        
+
+        public ClinkerController()
+        {
+            _storage = new ClinkerStorage();
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Clinker>> GetAll()
+        {
+            return _storage.GetClinkers();
+        }
+    }
+}
