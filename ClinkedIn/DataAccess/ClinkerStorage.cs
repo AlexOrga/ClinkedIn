b2b90081057b509c028d99cbb713bcebff78f138
Tom Dugan <tdugan85@gmail.com>
2018-10-01 21:11:51 -0500

    saved

--- ClinkedIn/DataAccess/ClinkerStorage.cs
+++ ClinkedIn/DataAccess/ClinkerStorage.cs
@@ -2,10 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ClinkedIn.Models;
 
 namespace ClinkedIn.DataAccess
 {
     public class ClinkerStorage
     {
+        static List<Clinker> _allClinkers = new List<Clinker>();
+        
+        public void Add(Clinker clinker)
+        {
+            clinker.Id = _allClinkers.Any() ? _allClinkers.Max(x => x.Id) + 1 : 1;
+            _allClinkers.Add(clinker);
+        }
+
+        public Clinker GetById(int id)
+        {
+            return _allClinkers.First(clinker => clinker.Id == id);
+        }
     }
 }
