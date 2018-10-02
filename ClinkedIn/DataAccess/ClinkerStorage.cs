e32f8bd3fd8ecde2851d14c8fba64a74f287cee9
Alex Orga <alexorga.dev@gmail.com>
2018-10-01 21:27:20 -0500

    added clinker info

--- ClinkedIn/DataAccess/ClinkerStorage.cs
+++ ClinkedIn/DataAccess/ClinkerStorage.cs
@@ -10,7 +10,11 @@ namespace ClinkedIn.DataAccess
     {
         static List<Clinker> _allClinkers = new List<Clinker>()
         {
-            
+            new Clinker() { Name = "Trevor", Nickname = "Lunch-Box", Charge = "Aggravated Cafeteria Robbery", Wallet = 50, SolitaryConfinement = true, Id = 1},
+            new Clinker() { Name = "Jimothy", Nickname = "Capone", Charge = "Tax Evasion", Wallet = 75, SolitaryConfinement = false, Id = 2},
+            new Clinker() { Name = "Doug", Nickname = "Queen Pookie", Charge = "Murder", Wallet = 20, SolitaryConfinement = true, Id = 3},
+            new Clinker() { Name = "John Jacob JingleHeimer", Nickname = "Schmitty", Charge = "Identity Fraud", Wallet = 35, SolitaryConfinement = false, Id = 4},
+            new Clinker() { Name = "Ralph", Nickname = "Five Head", Charge = "18th DUI", Wallet = 10, SolitaryConfinement = false, Id = 5}
         };
 
         public void Add(Clinker clinker)
