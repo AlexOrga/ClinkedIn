371a10da73b487e37921a3133ae35902292283be
Nathan Pabst <nathan.pabst@gmail.com>
2018-10-01 21:26:13 -0500

    added services file

--- ClinkedIn/Models/Services.cs
+++ ClinkedIn/Models/Services.cs
@@ -7,5 +7,8 @@ namespace ClinkedIn.Models
 {
     public class Services
     {
+        public string Name { get; set; }
+        public int Cost { get; set; }
+        public string Description { get; set; }
     }
 }
