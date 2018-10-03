0cec300cdf180ba5413e641d7effe384e6147deb
Alex Orga <alexorga.dev@gmail.com>
2018-10-02 20:58:37 -0500

    able to search users by interest

--- ClinkedIn/DataAccess/ClinkerStorage.cs
+++ ClinkedIn/DataAccess/ClinkerStorage.cs
@@ -24,6 +24,10 @@ namespace ClinkedIn.DataAccess
                     new Service {Name = "shank guard", Cost = 90, Description = "pull his pants down"},
                     new Service {Name = "clean", Cost = 15, Description = "i need to see my reflection"}
                 },
+                Interests =
+                {
+                    new Interest {Name = "Cards", Description = "Playing cards you idiot"},
+                },
                 Id = 1
             },
             new Clinker()
@@ -37,6 +41,10 @@ namespace ClinkedIn.DataAccess
                 {
                     new Service {Name = "shank guard", Cost = 90, Description = "pull his pants down"},
                     new Service {Name = "clean bathrooms", Cost = 15, Description = "clean the toilets with warden's toothbrush"}
+                },
+                Interests =
+                {
+                    new Interest {Name = "Making trashbag necklaces", Description = "Twisting up trash bags until they become prison jewelry"},
                 },
                 Id = 2
             },
@@ -52,6 +60,10 @@ namespace ClinkedIn.DataAccess
                     new Service {Name = "golden showers", Cost = 120, Description = "no explanation needed"},
                     new Service {Name = "shank guard", Cost = 90, Description = "pull his pants down"}
                 },
+                Interests =
+                {
+                    new Interest {Name = "Making wine", Description = "Taking all the fruit from a confidant in the cafeteria and leaving them in the toilet of a cell until they ferment into the worst wine you've ever had."},
+                },
                 Id = 3
             },
             new Clinker()
@@ -66,6 +78,10 @@ namespace ClinkedIn.DataAccess
                     new Service {Name = "golden showers", Cost = 120, Description = "no explanation needed"},
                     new Service {Name = "clean bathrooms", Cost = 15, Description = "clean the toilets with warden's toothbrush"}
                 },
+                Interests =
+                {
+                    new Interest {Name = "Working out", Description = "Gotta get big if you wanna run the yard."},
+                },
                 Id = 4
             },
             new Clinker()
@@ -78,6 +94,10 @@ namespace ClinkedIn.DataAccess
                 Services =
                 {
                     new Service {Name = "clean bathrooms", Cost = 15, Description = "clean the toilets with warden's toothbrush"}
+                },
+                Interests =
+                {
+                    new Interest {Name = "Cards", Description = "Playing cards you idiot"},
                 },
                 Id = 5
             }
