b467c7c50279da5f7647c7c08ba90a0ea780adfc
Nathan Pabst <nathan.pabst@gmail.com>
2018-10-02 19:01:15 -0500

    add clinker working

--- ClinkedIn/Controllers/ClinkerController.cs
+++ ClinkedIn/Controllers/ClinkerController.cs
@@ -7,43 +7,32 @@ using Microsoft.AspNetCore.Mvc;
 using ClinkedIn.DataAccess;
 using ClinkedIn.Models;
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 2310fc716007a3128553b94b168ba080d578bf9f
 namespace ClinkedIn.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
     public class ClinkerController : ControllerBase
     {
-<<<<<<< HEAD
 
-        static List<Clinker> Clinkers;
-        static ClinkerController()
-        {
-
-=======
         public readonly ClinkerStorage _storage;
         
 
         public ClinkerController()
         {
             _storage = new ClinkerStorage();
->>>>>>> 2310fc716007a3128553b94b168ba080d578bf9f
         }
 
         [HttpGet]
         public ActionResult<IEnumerable<Clinker>> GetAll()
         {
-<<<<<<< HEAD
-            return Clinkers;
-        }
-        
-
-=======
+            
             return _storage.GetClinkers();
         }
->>>>>>> 2310fc716007a3128553b94b168ba080d578bf9f
+
+        [HttpPost]
+        public void Add(Clinker clinker)
+        {
+            _storage.Add(clinker);
+        }
     }
 }
