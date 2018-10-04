f248f73f223764f766dad01f726e982380786bf4
Nathan Pabst <nathan.pabst@gmail.com>
2018-10-04 17:58:12 -0500

    add friends code in place. need to test in postman

--- ClinkedIn/Controllers/FriendsController.cs
+++ ClinkedIn/Controllers/FriendsController.cs
@@ -14,16 +14,20 @@ namespace ClinkedIn.Controllers
     public class FriendsController : ControllerBase
     {
 
-        [HttpPut("{id}/friendId")]
-        public IActionResult PutFriend(int id, int friendId)
+        [HttpPut("{id}/friends")]
+        public IActionResult PutFriend([FromQuery] int id, int friendId)
         {
             var storage = new ClinkerStorage();
-            var friend = storage.GetById(id);
+            var myInfo = storage.GetById(id);
+            var friend = storage.GetById(friendId);
 
             if (friend == null) return NotFound();
             friend.Friends.Add(id);
+            myInfo.Friends.Add(friendId);
             return Ok();
         }
+
+        
 
         
     }
